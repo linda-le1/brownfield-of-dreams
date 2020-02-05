89bb05d9d579df0663ce6a2e8fa4a73ca5e9985a
Linda Le <yueling1222@gmail.com>
2020-02-05 15:44:11 -0700

    Add Faraday connection for logged in user's repo with token present
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
   def show
-  end
+    # conn = Faraday.get(url: "https://api.github.com/users/") do |conn|
+    #   conn.token_auth('4ed4bd4ccb1151235aba19f009b4569cffca1d59')
+    # end
+
+    # response = Faraday.get("https://api.github.com/users/linda-le1/repos?access_token=4ed4bd4ccb1151235aba19f009b4569cffca1d59")
+    github_token = ENV["GITHUB_TOKEN"]
+    response = Faraday.get("https://api.github.com/user/repos?access_token=#{github_token}")
+    @repos = JSON.parse(response.body)
+    # first-repo = repos.first["name"]
+    # first-repo = repos.first["html_url"]
+
+    end
 
   def new
     @user = User.new
