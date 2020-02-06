e1851e6082c03176d5a3fdaae0eb1e53340ddb3a
Alison Vermeil <alison.minton@gmail.com>
2020-02-05 20:47:08 -0700

    Change first it block to use VCR, add notes for further testing
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>

--- spec/features/user/github/user_can_see_repos_spec.rb
+++ spec/features/user/github/user_can_see_repos_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 describe 'A registered user' do
-    before :each do
-        VCR.turn_off!
-        WebMock.allow_net_connect!
-    end
-    it 'can view repos with token' do
+    scenario 'can view repos with token', :vcr do
         #need repos
         #log the user in
 
@@ -18,18 +14,27 @@ describe 'A registered user' do
             expect(page).to have_content("Github Repos")
 
             #repos 1 - 5 links here
-            expect(page).to have_link("Repo One Name Here")
-            expect(page).to have_link("Repo Two Name Here")
-            expect(page).to have_link("Repo Three Name Here")
-            expect(page).to have_link("Repo Four Name Here")
-            expect(page).to have_link("Repo Five Name Here")
+            # is order important?
+            expect(page).to have_link("monster_shop_part_1")
+            expect(page).to have_link("activerecord-obstacle-course")
+            expect(page).to have_link("adopt_dont_shop")
+            expect(page).to have_link("adopt_dont_shop_part_two")
+            expect(page).to have_link("futbol")
         end
 
     end
 
     xit 'can only repos belonging to logged in user' do
+      repo_1 = Repo.new("Not my repo", "not my repo URL")
+      repo_2 = Repo.new("Not my repo either", "not my repo URL")
+      repo_3 = Repo.new("Nor this one", "not my repo URL")
+
+      #expect that you don't see any of these repos listed??
+      #does that even make sense?
     end
 
     xit 'cannot see repos without token' do
+      # i think that we need to add a column to the model before we can
+      # really start testing other users
     end
 end
