c432762ea30dc5ce02afaa0b75ce31420fbaaf30
Alison Vermeil <alison.minton@gmail.com>
2020-02-06 16:32:11 -0700

    Refactor tests to use new fixture data
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>

--- spec/features/user/github/user_can_see_repos_spec.rb
+++ spec/features/user/github/user_can_see_repos_spec.rb
@@ -2,34 +2,36 @@ require 'rails_helper'
 
 describe 'A registered user' do
     scenario 'can view repos with token' do
-        repo_fixture = File.read('spec/fixtures/repos.json')
+        repo_fixture = File.read('spec/fixtures/repos_ali.json')
 
         stub_request(:get, "https://api.github.com/user/repos?page=1&per_page=5").
         to_return(status: 200, body: repo_fixture)
 
-        user = create(:user)
+        user = create(:user, github_token: ENV["GITHUB_TOKEN"])
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
         visit '/dashboard'
 
         within "#github-repos" do
+          expect(page).to have_css(".repo", count: 5)
             expect(page).to have_content("Github Repos")
-            expect(page).to have_link("monster_shop_part_1")
             expect(page).to have_link("activerecord-obstacle-course")
+            expect(page).to have_link("backend_module_0_capstone")
+            expect(page).to have_link("battleship")
             expect(page).to have_link("adopt_dont_shop")
             expect(page).to have_link("adopt_dont_shop_part_two")
-            expect(page).to have_link("futbol")
         end
     end
 
     scenario 'can only repos belonging to logged in user' do
-      repo_fixture = File.read('spec/fixtures/repos.json')
+      repo_fixture = File.read('spec/fixtures/repos_ali.json')
 
-      stub_request(:get, "https://api.github.com/user/repos?page=1&per_page=5").
+      stub_request(:get, "https://api.github.com/user/repos?page=1&per_page=5&affiliation=owner").
       to_return(status: 200, body: repo_fixture)
 
-      user = create(:user)
+      user = create(:user, github_token: ENV["GITHUB_TOKEN"])
+      user_2 = create(:user, github_token: ENV["GIT_HUB_TOKEN_LINDA"])
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -37,11 +39,12 @@ describe 'A registered user' do
 
       within "#github-repos" do
           expect(page).to have_content("Github Repos")
-          expect(page).to have_link("monster_shop_part_1")
-          expect(page).to have_link("activerecord-obstacle-course")
-          expect(page).to have_link("adopt_dont_shop")
-          expect(page).to have_link("adopt_dont_shop_part_two")
-          expect(page).to have_link("futbol")
+          expect(page).to have_link("activerecord-obstacle-course", href: "https://github.com/mintona/activerecord-obstacle-course")
+          expect(page).to have_link("adopt_dont_shop", href: "https://github.com/mintona/adopt_dont_shop")
+          expect(page).to have_link("adopt_dont_shop_part_two", href: "https://github.com/mintona/adopt_dont_shop_part_two")
+          expect(page).to have_link("backend_module_0_capstone", href: "https://github.com/mintona/backend_module_0_capstone")
+          expect(page).to have_link("battleship", href: "https://github.com/mintona/battleship")
+#expect page to not have any data from the other file 
       end
     end
 
