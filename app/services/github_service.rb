90f3117e443e02d16af07e937281a6897f54e334
Alison Vermeil <alison.minton@gmail.com>
2020-02-06 14:53:54 -0700

    Create github service
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,9 +1,18 @@
 class GithubService
   def get_repos
-    conn = Faraday.new(url: "https://api.github.com/")
-    github_token = ENV["GITHUB_TOKEN"] # variable above would replace this
-    conn.basic_auth(nil, github_token)
-    response = conn.get("/user/repos?page=1&per_page=5")
-    repos = JSON.parse(response.body, symbolize_names: true)
+    get_json("/user/repos?page=1&per_page=5")
   end
+
+  private
+    def conn
+      connection = Faraday.new(url: "https://api.github.com/")
+      github_token = ENV["GITHUB_TOKEN"]
+      connection.basic_auth(nil, github_token)
+      connection
+    end
+
+    def get_json(url)
+      response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
+    end
 end
