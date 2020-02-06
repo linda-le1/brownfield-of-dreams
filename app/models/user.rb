ed25ef557e9eb1e6b4c12e1e1363f17a63816539
Alison Vermeil <alison.minton@gmail.com>
2020-02-06 16:32:48 -0700

    Refactor for readability
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
 
+  validates_presence_of :first_name, :last_name, :password
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
-  validates_presence_of :first_name
-  enum role: [:default, :admin]
+  # validates_presence_of :github_token, presence: false
+  
   has_secure_password
+
+  enum role: [:default, :admin]
 end
