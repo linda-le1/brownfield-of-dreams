1027a5f41607bda914bb8483a06036c5ed2350a9
Alison Vermeil <alison.minton@gmail.com>
2020-02-06 14:17:32 -0700

    Change test to not use VCR
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>

--- spec/features/user/github/user_can_see_repos_spec.rb
+++ spec/features/user/github/user_can_see_repos_spec.rb
@@ -1,40 +1,47 @@
 require 'rails_helper'
 
 describe 'A registered user' do
-    scenario 'can view repos with token', :vcr do
-        #need repos
-        #log the user in
+    scenario 'can view repos with token' do
+        repo_fixture = File.read('spec/fixtures/repos.json')
+
+        stub_request(:get, "https://api.github.com/user/repos?page=1&per_page=5").
+        to_return(status: 200, body: repo_fixture)
 
         user = create(:user)
+
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
         visit '/dashboard'
 
         within "#github-repos" do
             expect(page).to have_content("Github Repos")
-
-            #repos 1 - 5 links here
-            # is order important?
             expect(page).to have_link("monster_shop_part_1")
             expect(page).to have_link("activerecord-obstacle-course")
             expect(page).to have_link("adopt_dont_shop")
             expect(page).to have_link("adopt_dont_shop_part_two")
             expect(page).to have_link("futbol")
         end
-
     end
 
     xit 'can only repos belonging to logged in user' do
-      repo_1 = Repo.new("Not my repo", "not my repo URL")
-      repo_2 = Repo.new("Not my repo either", "not my repo URL")
-      repo_3 = Repo.new("Nor this one", "not my repo URL")
+      # repo_1 = Repo.new("Not my repo", "not my repo URL")
+      # repo_2 = Repo.new("Not my repo either", "not my repo URL")
+      # repo_3 = Repo.new("Nor this one", "not my repo URL")
 
       #expect that you don't see any of these repos listed??
       #does that even make sense?
     end
 
     xit 'cannot see repos without token' do
+      # user = create(:github_token = "")
       # i think that we need to add a column to the model before we can
       # really start testing other users
+
+      # need to get out the hard coded token for this test
+      # would you stub this out?
+      # allow ENV["GITHUB_TOKEN"]. to eq("")
     end
 end
+
+#create seeds using figaro, 2 users: one with linda, one with mine
+#testing : 1 user w/ a correct token and one without
