59b96298e6900e6675e9abad8f2d6a75f154a959
Alison Vermeil <alison.minton@gmail.com>
2020-02-06 17:04:40 -0700

    Remove unnecessary assertion
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of(:first_name)}
     it {should validate_presence_of(:last_name)}
     it {should validate_presence_of(:password)}
-    it {should validate_presence_of(:github_token)}
   end
 
   describe 'attributes' do
