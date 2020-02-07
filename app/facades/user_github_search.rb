a5ea7fed2529f08c48045174ed6d0fe35edfd902
Linda Le <yueling1222@gmail.com>
2020-02-07 10:04:19 -0700

    Add get json for followers in github service and in facade with stub files
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>

---
+++ app/facades/user_github_search.rb
@@ -0,0 +1,29 @@
+class UserGithubSearch
+  def initialize(token)
+    @token = token
+  end
+
+  def repos
+    return @repos if @repos
+    service = GithubService.new(@token)
+    @repos = service.get_repos.map do |repo|
+      Repository.new(repo)
+    end
+  end
+
+  def followers
+    return @followers if @followers
+    service = GithubService.new(@token)
+    @followers = service.get_followers.map do |follower|
+      Follower.new(follower)
+    end
+  end
+
+  def following
+    return @following if @following
+    service = GithubService.new(@token)
+    @following = service.get_following.map do |following|
+      Following.new(following)
+    end
+  end
+end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -7,6 +7,14 @@ class GithubService
     get_json("/user/repos?page=1&per_page=5&affiliation=owner")
   end
 
+  def get_followers
+    get_json("/user/followers")
+  end
+
+  def get_following
+    get_json("/user/following")
+  end
+
   private
     def conn
       connection = Faraday.new(url: "https://api.github.com/")
