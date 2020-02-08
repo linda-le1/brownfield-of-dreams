0db8935be843506f8b4305ae818c98c9b2868612
Alison Vermeil <alison.minton@gmail.com>
2020-02-08 16:38:44 -0700

    Refactor welcome controller to check for current_user, then display correct tutorials with model method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,16 @@ class WelcomeController < ApplicationController
     if params[:tag]
       @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
     else
-      @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+      display_tutorials
     end
   end
+
+  private
+    def display_tutorials
+      if current_user
+        @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+      else
+        @tutorials = Tutorial.not_classroom_content.paginate(:page => params[:page], :per_page => 5)
+      end
+    end
 end
