9bab374f3b28c34d82154fb3cfa7637727ca372b
Alison Vermeil <alison.minton@gmail.com>
2020-02-08 16:37:37 -0700

    Add assertions to tutorial_spec and test for model method not_classroom_content

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe "validations" do
+    it { should have_many :videos }
+    #should there be more assertions here for the
+    #taggable and nested_attributes?
+  end
+
+  describe "model methods" do
+    describe "#not_classroom_content" do
+      it "displays only tutorials where classroom attribute is false" do
+        create_list(:tutorial, 4)
+        tutorial5 = create(:tutorial, classroom: true)
+
+        result = Tutorial.not_classroom_content
+
+        expect(result.length).to eq(4)
+        expect(result.include?(tutorial5)).to eq(false)
+      end
+    end
+  end
 end
