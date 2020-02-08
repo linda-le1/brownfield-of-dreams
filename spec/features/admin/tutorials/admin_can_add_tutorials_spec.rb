ac367e82490e6caba0faf88b4d5428be7ebea08b
Linda Le <yueling1222@gmail.com>
2020-02-08 16:24:19 -0700

    Add spec test for adding tutorial

--- spec/features/admin/tutorials/admin_can_add_tutorials_spec.rb
+++ spec/features/admin/tutorials/admin_can_add_tutorials_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 describe "As an admin" do
     scenario "can add a tutorial" do
         admin = create(:user, role: :admin)
+
+        visit new_admin_tutorial_path
+
+        fill_in :title, with: "OAuth Authentication with Github"
+        fill_in :description, with: "Learn how to implemented OAuth Github Authentication into your Rails app."
+        fill_in :thumbnail, with: "https://i.ytimg.com/vi/c2UnIQ3LRnM/hqdefault.jpg"
+
+        click_button "Save"
+
+        expect(current_path).to eq(tutorial_path)
+        expect(page).to have_content("Successfully created tutorial.")
     end
 end
-
-
-# When I visit '/admin/tutorials/new'
-# And I fill in 'title' with a meaningful name
-# And I fill in 'description' with a some content
-# And I fill in 'thumbnail' with a valid YouTube thumbnail
-# And I click on 'Save'
-# Then I should be on '/tutorials/{NEW_TUTORIAL_ID}'
-# And I should see a flash message that says "Successfully created tutorial."
