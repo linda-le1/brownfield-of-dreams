252a7ff0ab86720f3321c7b24e6eee64d5211a90
Alison Vermeil <alison.minton@gmail.com>
2020-02-08 16:06:56 -0700

    Add robustness to old test and add new test for only seeing non classroom content

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -15,11 +15,39 @@ describe 'Visitor' do
 
       expect(page).to have_css('.tutorial', count: 2)
 
-      within(first('.tutorials')) do
-        expect(page).to have_css('.tutorial')
+      within("#tutorial-#{tutorial1.id}") do
         expect(page).to have_css('.tutorial-description')
         expect(page).to have_content(tutorial1.title)
         expect(page).to have_content(tutorial1.description)
+      end
+
+      within("#tutorial-#{tutorial2.id}") do
+        expect(page).to have_css('.tutorial-description')
+        expect(page).to have_content(tutorial2.title)
+        expect(page).to have_content(tutorial2.description)
+      end
+    end
+
+    it "does not see tutorials marked as 'classroom content'" do
+      tutorial1 = create(:tutorial)
+      tutorial2 = create(:tutorial, classroom: true)
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video2 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+      video4 = create(:video, tutorial_id: tutorial2.id)
+
+      visit root_path
+
+      expect(page).to have_css('.tutorial', count: 1)
+
+      within('.tutorials') do
+        expect(page).to have_css("#tutorial-#{tutorial1.id}")
+        expect(page).to have_content(tutorial1.title)
+        expect(page).to have_content(tutorial1.description)
+        expect(page).to_not have_css("#tutorial-#{tutorial2.id}")
+        expect(page).to_not have_content(tutorial2.title)
+        expect(page).to_not have_content(tutorial2.description)
       end
     end
   end
