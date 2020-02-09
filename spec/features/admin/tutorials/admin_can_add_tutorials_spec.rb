1ba1a7319dc377513ac9f5bf91ff24ecb89a9c92
Linda Le <yueling1222@gmail.com>
2020-02-08 17:11:45 -0700

    Add sad path testing for creating tutorial

--- spec/features/admin/tutorials/admin_can_add_tutorials_spec.rb
+++ spec/features/admin/tutorials/admin_can_add_tutorials_spec.rb
@@ -18,4 +18,20 @@ describe "As an admin" do
         expect(current_path).to eq(tutorial_path(tutorial.id))
         expect(page).to have_content("Successfully created tutorial.")
     end
+
+    scenario "cannot add a tutorial if bad information is submitted" do
+        admin = create(:user, role: :admin)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+        visit "/admin/tutorials/new"
+
+        fill_in :tutorial_title, with: " "
+        fill_in :tutorial_description, with: "Learn how to implemented OAuth Github Authentication into your Rails app."
+        fill_in :tutorial_thumbnail, with: "https://i.ytimg.com/vi/c2UnIQ3LRnM/hqdefault.jpg"
+
+        click_button "Save"
+
+        expect(page).to have_content("Tutorial was not successfully created.")
+        expect(page).to have_content("Title can't be blank")
+    end
 end
