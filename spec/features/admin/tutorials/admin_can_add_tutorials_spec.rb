07efa2a89b2d5d28c838371b56ffb66a2665fa09
Linda Le <yueling1222@gmail.com>
2020-02-08 17:05:09 -0700

    Fix variables in spec test to add tutorial

--- spec/features/admin/tutorials/admin_can_add_tutorials_spec.rb
+++ spec/features/admin/tutorials/admin_can_add_tutorials_spec.rb
@@ -3,16 +3,19 @@ require 'rails_helper'
 describe "As an admin" do
     scenario "can add a tutorial" do
         admin = create(:user, role: :admin)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-        visit new_admin_tutorial_path
+        visit "/admin/tutorials/new"
 
-        fill_in :title, with: "OAuth Authentication with Github"
-        fill_in :description, with: "Learn how to implemented OAuth Github Authentication into your Rails app."
-        fill_in :thumbnail, with: "https://i.ytimg.com/vi/c2UnIQ3LRnM/hqdefault.jpg"
+        fill_in :tutorial_title, with: "OAuth Authentication with Github"
+        fill_in :tutorial_description, with: "Learn how to implemented OAuth Github Authentication into your Rails app."
+        fill_in :tutorial_thumbnail, with: "https://i.ytimg.com/vi/c2UnIQ3LRnM/hqdefault.jpg"
 
         click_button "Save"
 
-        expect(current_path).to eq(tutorial_path)
+        tutorial = Tutorial.last
+
+        expect(current_path).to eq(tutorial_path(tutorial.id))
         expect(page).to have_content("Successfully created tutorial.")
     end
 end
