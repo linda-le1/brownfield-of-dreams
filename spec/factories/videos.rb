8eeb4ac028c153007b323f46db87bd59b84feffb
Linda Le <yueling1222@gmail.com>
2020-02-09 17:25:35 -0700

    Add required attributes to video factory file

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     title { Faker::Pokemon.name }
     description { Faker::SiliconValley.motto }
     video_id { Faker::Crypto.md5 }
+    thumbnail { 'http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg' }
+    position { 1 }
     tutorial
   end
 end
