970500e0d10fbce4e5c7cd2254c8c78d3314c350
Linda Le <yueling1222@gmail.com>
2020-02-11 12:22:19 -0700

    Remove prys from code
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,7 +3,6 @@ class FriendsController < ApplicationController
     friender = current_user
     friendee = User.find_by(uid: params[:friendee_uid])
     friender.friendees << friendee
-    binding.pry
     flash[:success] = "You have added a friend!"
     redirect_to '/dashboard'
     # Friend.create(friender_id: friender, friendee_id: friendee)
