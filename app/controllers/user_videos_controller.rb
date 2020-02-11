25875f62c90a4f7e09360b23e1f26c2b4383c180
Alison Vermeil <alison.minton@gmail.com>
2020-02-10 19:56:19 -0700

    Indent private methods for readability

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -14,15 +14,15 @@ class UserVideosController < ApplicationController
 
   private
 
-  def user_video_params
-    params.permit(:user_id, :video_id)
-  end
-
-  def attempt_bookmark(user_video)
-    if current_user.user_videos.find_by(video_id: user_video.video_id)
-      flash[:error] = "Already in your bookmarks"
-    elsif user_video.save
-      flash[:success] = "Bookmark added to your dashboard!"
+    def user_video_params
+      params.permit(:user_id, :video_id)
     end
-  end
+
+    def attempt_bookmark(user_video)
+      if current_user.user_videos.find_by(video_id: user_video.video_id)
+        flash[:error] = "Already in your bookmarks"
+      elsif user_video.save
+        flash[:success] = "Bookmark added to your dashboard!"
+      end
+    end
 end
