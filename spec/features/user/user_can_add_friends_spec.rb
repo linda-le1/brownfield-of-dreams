e3032286c924837a0c92acddbde7da58f913aecd
Linda Le <yueling1222@gmail.com>
2020-02-10 17:21:21 -0700

    Add test for adding friends
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>

--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
 
 describe 'A registered user with a token' do
-    it 'can add friends who are also users with tokens' do
+    describe 'can add friends who are also users of our app with tokens' do
+      it "by clicking on the add friend button" do
+
+      followers_fixture = File.read('spec/fixtures/followers_linda.json')
+
+      stub_request(:get, "https://api.github.com/user/followers").
+      to_return(status: 200, body: followers_fixture)
+
+      linda = create(:user, github_token: ENV["GITHUB_TOKEN_LINDA"], uid: 54052410)
+      ali = create(:user, github_token: ENV["GITHUB_TOKEN_ALI"], uid: 51250305)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(linda)
+
+      visit '/dashboard'
+
+      within("#github-followers") do
+        within("#follower-danmoran-pro") do
+          expect(page).to have_link("danmoran-pro")
+          expect(page).to_not have_button("Add as Friend")
+        end
+        within("#follower-mintona") do
+          expect(page).to have_link("mintona")
+          expect(page).to have_button("Add as Friend")
+          click_on("Add as Friend")
+        end
+      end
+
+      expect(page).to have_content("You have added a friend!")
+      expect(current_path).to eq("/dashboard")
+      expect(page).to have_css(".friends")
+      within(".friends") do
+        expect(page).to have_content("mintona")
+      end
         # linda with a token is our logged in user
         # this person needs 2 followers on github
         # mintona with a token is 1 follower
@@ -18,4 +50,5 @@ describe 'A registered user with a token' do
         #section on dashboard of all users that linda has friended
         #friendship should not go through if id invalid (see story)
     end
+  end
 end
