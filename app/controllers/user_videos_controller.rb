abf03745887d668e65112ff6ca966e69463eb40b
Alison Vermeil <alison.minton@gmail.com>
2020-02-10 19:32:10 -0700

    Add conditional to user videos controller to check for current user with flash message if not

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -3,13 +3,17 @@ class UserVideosController < ApplicationController
   end
 
   def create
-    user_video = UserVideo.new(user_video_params)
-    if current_user.user_videos.find_by(video_id: user_video.video_id)
-      flash[:error] = "Already in your bookmarks"
-    elsif user_video.save
-      flash[:success] = "Bookmark added to your dashboard!"
+    if !current_user
+      flash[:notice] = "User must login to bookmark videos."
+    else
+      user_video = UserVideo.new(user_video_params)
+      attempt_bookmark(user_video)
+      # if current_user.user_videos.find_by(video_id: user_video.video_id)
+      #   flash[:error] = "Already in your bookmarks"
+      # elsif user_video.save
+      #   flash[:success] = "Bookmark added to your dashboard!"
+      # end
     end
-
     redirect_back(fallback_location: root_path)
   end
 
@@ -17,5 +21,13 @@ class UserVideosController < ApplicationController
 
   def user_video_params
     params.permit(:user_id, :video_id)
+  end
+
+  def attempt_bookmark(user_video)
+    if current_user.user_videos.find_by(video_id: user_video.video_id)
+      flash[:error] = "Already in your bookmarks"
+    elsif user_video.save
+      flash[:success] = "Bookmark added to your dashboard!"
+    end
   end
 end
