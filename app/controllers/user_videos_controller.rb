0e011ce2f50640c36b61f2319921239017e14f70
Alison Vermeil <alison.minton@gmail.com>
2020-02-10 19:53:47 -0700

    Refactor show method into helper methods

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -8,11 +8,6 @@ class UserVideosController < ApplicationController
     else
       user_video = UserVideo.new(user_video_params)
       attempt_bookmark(user_video)
-      # if current_user.user_videos.find_by(video_id: user_video.video_id)
-      #   flash[:error] = "Already in your bookmarks"
-      # elsif user_video.save
-      #   flash[:success] = "Bookmark added to your dashboard!"
-      # end
     end
     redirect_back(fallback_location: root_path)
   end
