0c7a002120c1f06fe86d541c8857a8258af0d3db
Linda Le <yueling1222@gmail.com>
2020-02-12 11:36:15 -0700

    Remove callback line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,6 +105,4 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
-  # Do not swallow errors in after_commit/after_rollback callbacks.
-  config.active_record.raise_in_transactional_callbacks = true
 end
