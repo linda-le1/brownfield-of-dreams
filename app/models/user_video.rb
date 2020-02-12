daa39e1ea92846e93537342e9f111bd35adbd7bc
Alison Vermeil <alison.minton@gmail.com>
2020-02-11 21:11:00 -0700

    Add video titles method

--- app/models/user_video.rb
+++ app/models/user_video.rb
@@ -1,4 +1,8 @@
 class UserVideo < ApplicationRecord
   belongs_to :video, foreign_key: 'video_id'
   belongs_to :user, foreign_key: 'user_id'
+
+  def self.video_titles(user_id)
+    joins(video: :tutorial).select("videos.title, videos.id AS video_id, tutorials.id AS tutorial_id").where(user_id: user_id).order("tutorials.title, videos.position")
+  end
 end
