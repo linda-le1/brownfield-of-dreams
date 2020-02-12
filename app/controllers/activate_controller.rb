3108beacdd086602bef3abf5c00e2addfdf39e99
Linda Le <yueling1222@gmail.com>
2020-02-12 00:01:04 -0700

    Update spacing

--- app/controllers/activate_controller.rb
+++ app/controllers/activate_controller.rb
@@ -1,5 +1,6 @@
 class ActivateController < ApplicationController
     def create
+        user = User.find(params[:id])
         user.update(active?: true)
         redirect_to "/dashboard"
     end
