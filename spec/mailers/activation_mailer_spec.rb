31d6f3bcf060140c337f94b73b8bb878af1f88f2
Linda Le <yueling1222@gmail.com>
2020-02-11 22:38:48 -0700

    Add test for mailer spec

---
+++ spec/mailers/activation_mailer_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+Describe ActivationMailer, type: :mailer do
+    describe "When a user registers an account and gets an activation email" do
+        before :each do
+            user = create(:user, email: "example@gmail.com")
+            email = ActivationMailer.send_activation_letter(@user)
+        end
+        it "has the following subject" do
+            expect(email.subject).to eql("Activate Your Account")
+        end
+
+        it "has the correct sender's email address" do
+            expect(email.from).to eql("no-reply@brownfieldofdreams.com")
+        end
+
+        it "has the correct receiving user's email address" do
+            expect(email.to).to eql("#{user.email}")
+        end
+
+        it "has the activation url" do
+            expect(email.body).to eql("/activate")
+        end
+    end
+end
