8c0b07e1327e0791134d130e03c6c32e1d0a09e0
Alison Vermeil <alison.minton@gmail.com>
2020-02-11 21:25:03 -0700

    Remove comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
   def show
-    # if current_user.github_token?
       render locals: {
         user_dashboard_facade: UserDashboardFacade.new(current_user)
       }
-    # end
   end
 
   def new
