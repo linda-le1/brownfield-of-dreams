9862ab51317b266041e582969400cb5f55d04347
Alison Vermeil <alison.minton@gmail.com>
2020-02-11 17:27:50 -0700

    Add test for new user status method
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,4 +42,16 @@ RSpec.describe User, type: :model do
       expect(admin.admin?).to be_truthy
     end
   end
+
+  describe 'model methods' do
+    describe '#status' do
+      it 'displays the status of the user' do
+        user_1 = create(:user)
+        user_2 = create(:user, active?: true)
+
+        expect(user_1.status).to eq("Inactive")
+        expect(user_2.status).to eq("Active")
+      end
+    end
+  end
 end
