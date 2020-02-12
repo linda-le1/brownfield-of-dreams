b5f3f5b96d908854711d975391153edceccbfd4f
Alison Vermeil <alison.minton@gmail.com>
2020-02-11 17:24:41 -0700

    Add test for email activation post registration
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>

--- spec/features/vistors/visitor_can_register_spec.rb
+++ spec/features/vistors/visitor_can_register_spec.rb
@@ -1,36 +1,52 @@
 require 'rails_helper'
+describe 'as a visitor', :js do
+  describe 'can register for an account' do
+    it 'by entering valid information' do
+      email = 'jimbob@aol.com'
+      first_name = 'Jim'
+      last_name = 'Bob'
+      password = 'password'
+      password_confirmation = 'password'
+      visit '/'
+      click_on 'Sign In'
+      expect(current_path).to eq(login_path)
+      click_on 'Sign up now.'
+      expect(current_path).to eq(new_user_path)
+      fill_in 'user[email]', with: email
+      fill_in 'user[first_name]', with: first_name
+      fill_in 'user[last_name]', with: last_name
+      fill_in 'user[password]', with: password
+      fill_in 'user[password_confirmation]', with: password
+      click_on'Create Account'
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content(email)
+      expect(page).to have_content(first_name)
+      expect(page).to have_content(last_name)
+      expect(page).to_not have_content('Sign In')
+      expect(page).to have_content("Logged in as #{first_name}.")
+      expect(page).to have_content("This account has not yet been activated. Please check your email.")
+      expect(page).to have_content("Status: Inactive")
+    end
 
-describe 'vister can create an account', :js do
-  it ' visits the home page' do
-    email = 'jimbob@aol.com'
-    first_name = 'Jim'
-    last_name = 'Bob'
-    password = 'password'
-    password_confirmation = 'password'
-
-    visit '/'
-
-    click_on 'Sign In'
-
-    expect(current_path).to eq(login_path)
-
-    click_on 'Sign up now.'
-
-    expect(current_path).to eq(new_user_path)
-
-    fill_in 'user[email]', with: email
-    fill_in 'user[first_name]', with: first_name
-    fill_in 'user[last_name]', with: last_name
-    fill_in 'user[password]', with: password
-    fill_in 'user[password_confirmation]', with: password
-
-    click_on'Create Account'
-
-    expect(current_path).to eq(dashboard_path)
-
-    expect(page).to have_content(email)
-    expect(page).to have_content(first_name)
-    expect(page).to have_content(last_name)
-    expect(page).to_not have_content('Sign In')
+    it 'will display a flash message if bad information is submitted' do
+      user = create(:user, email: "example@gmail.com" )
+      email = 'example@gmail.com'
+      first_name = 'Jim'
+      last_name = 'Bob'
+      password = 'password'
+      password_confirmation = 'password'
+      visit '/'
+      click_on 'Sign In'
+      expect(current_path).to eq(login_path)
+      click_on 'Sign up now.'
+      expect(current_path).to eq(new_user_path)
+      fill_in 'user[email]', with: email
+      fill_in 'user[first_name]', with: first_name
+      fill_in 'user[last_name]', with: last_name
+      fill_in 'user[password]', with: password
+      fill_in 'user[password_confirmation]', with: password
+      click_on 'Create Account'
+      expect(page).to have_content("Username already exists")
+    end
   end
 end
