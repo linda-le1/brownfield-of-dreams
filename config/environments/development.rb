53c22ed94d828969f6a9d4d27c23eff1e69d62f3
Linda Le <yueling1222@gmail.com>
2020-02-12 00:00:21 -0700

    Update routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = true
   # Settings specified here will take precedence over those in config/application.rb.
@@ -62,4 +60,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
-  get '/activate', to: "activate#create"
+  get '/activate/:id', to: "activate#create"
 
   get '/dashboard', to: 'users#show'
   get '/about', to: 'about#show'
