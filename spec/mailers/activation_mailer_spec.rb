e0b9be2a59bc23cd736eeb82f879dd17b9318a7d
Linda Le <yueling1222@gmail.com>
2020-02-11 23:24:43 -0700

    Update method names

--- spec/mailers/activation_mailer_spec.rb
+++ spec/mailers/activation_mailer_spec.rb
@@ -2,23 +2,24 @@ require "rails_helper"
 describe ActivationMailer, type: :mailer do
     describe "When a user registers an account and gets an activation email" do
         before :each do
-            user = create(:user, email: "example@gmail.com")
-            email = ActivationMailer.send_activation_letter(@user)
+            @user = create(:user, email: "example@gmail.com")
+            @email = ActivationMailer.activate_account(@user)
         end
+
         it "has the following subject" do
-            expect(email.subject).to eql("Activate Your Account")
+            expect(@email.subject).to eql("Activate Your Account")
         end
 
         it "has the correct sender's email address" do
-            expect(email.from).to eql("no-reply@brownfieldofdreams.com")
+            expect(@email.from).to eql("no-reply@brownfieldofdreams.com")
         end
 
         it "has the correct receiving user's email address" do
-            expect(email.to).to eql("#{user.email}")
+            expect(@email.to).to eql("#{@user.email}")
         end
 
         it "has the activation url" do
-            expect(email.body).to eql("/activate")
+            expect(@email.body).to eql("/activate")
         end
     end
 end
