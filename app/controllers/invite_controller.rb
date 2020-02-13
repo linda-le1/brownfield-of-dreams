54529e75bf545a1da78797b7804dcc9bb421fa58
Alison Vermeil <alison.minton@gmail.com>
2020-02-12 18:38:24 -0700

    Add create method with happy and sad flash notices
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -3,8 +3,27 @@ class InviteController < ApplicationController
   end
 
   def create
-    # response comes back as "404 Not Found" if the username isn't a real github name
     service = GithubService.new(current_user.github_token)
 
+    handle = params[:github_handle]
+
+    if service.user_exists?(handle)
+      invitee = service.get_user(handle)
+      invitee_email = invitee[:email]
+      invitee_name = invitee[:name]
+      if invitee_email == nil
+        flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+        render :new
+      else
+
+      InviteMailer.invite_user(invitee_email, invitee_name, current_user.first_name).deliver_now
+      flash[:success] = "Successfully sent invite!"
+
+      redirect_to dashboard_path
+      end
+    else
+      flash[:error] = "There is no Github user with that handle."
+      render :new 
+    end
   end
 end
