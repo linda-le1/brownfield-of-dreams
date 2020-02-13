fb87cb9685f3e81252157cff48724e7592e5e775
Alison Vermeil <alison.minton@gmail.com>
2020-02-12 18:37:50 -0700

    Add sad path tests for invites
    Co-Authored-By: Linda Le <linda-le1@users.noreply.github.com>

--- spec/features/user/github/user_can_invite_a_person_spec.rb
+++ spec/features/user/github/user_can_invite_a_person_spec.rb
@@ -16,14 +16,17 @@ RSpec.describe "As a logged in user" do
 
         fill_in :github_handle, with: 'mintona'
 
-        click_button 'Send Invite'
+        expect do
+          click_button 'Send Invite'
+        end.
+        to change { ActionMailer::Base.deliveries.count }.by(1)
 
         expect(current_path).to eq(dashboard_path)
 
         expect(page).to have_content('Successfully sent invite!')
       end
 
-      xscenario "I am alerted if they don't have email in github" do
+      scenario "I am alerted if they don't have email in github" do
         user = create(:user, github_token: ENV["GITHUB_TOKEN_LINDA"])
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -34,16 +37,15 @@ RSpec.describe "As a logged in user" do
 
         expect(current_path).to eq('/invite')
 
-        fill_in 'Github Handle', with: 'mintona'
+        fill_in :github_handle, with: 'danmoran-pro'
 
         click_button 'Send Invite'
 
-        expect(current_path).to eq(dashboard_path)
-
         expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
+        expect(page).to have_button 'Send Invite'
       end
 
-      xscenario "I am alerted if I enter an invalid github handle" do
+      scenario "I am alerted if I enter an invalid github handle" do
         user = create(:user, github_token: ENV["GITHUB_TOKEN_LINDA"])
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -58,9 +60,23 @@ RSpec.describe "As a logged in user" do
 
         click_button 'Send Invite'
 
-        expect(current_path).to eq(dashboard_path)
-
         expect(page).to have_content("There is no Github user with that handle.")
+        expect(page).to have_button 'Send Invite'
+      end
+    end
+  end
+
+  describe "As the invitee" do
+    describe "when I click the link from the email invite" do
+      scenario "I am taken to a signup page" do
+        visit "/signup"
+
+        expect(page).to have_content('Register')
+        expect(page).to have_content('Email')
+        expect(page).to have_content('First name')
+        expect(page).to have_content('Last name')
+        expect(page).to have_content('Password')
+        expect(page).to have_button('Create Account')
       end
     end
   end
